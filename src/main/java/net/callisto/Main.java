12c0c1b0d8134c5be25a69be90ee0015eb660730
Callisto95 <luluklzde@gmail.com>
2026-02-06 18:10:26 +0100

    extract to functions and use map for lookup

--- src/main/java/net/callisto/Main.java
+++ src/main/java/net/callisto/Main.java
@@ -1,12 +1,18 @@
 package net.callisto;
 
 import javax.imageio.*;
+import java.awt.*;
 import java.awt.geom.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.*;
+import java.util.List;
 
 public class Main {
+    private static final int PRIMARY_COLOUR   = 0xFF0000;
+    private static final int SECONDARY_COLOUR = 0x000000;
+    private static final int SCALE            = 25;
+    
     public static void main(String[] args) throws IOException {
         final List<String> lines = Arrays.asList(args);
         
@@ -15,52 +21,86 @@ public class Main {
             System.exit(0);
         }
         
-        final int lineCount = lines.size();
+        List<List<byte[][]>> convertedLines = convertLines(lines);
         
-        final int charCount = lines // NOSONAR: always available
+        final int lineCount = lines.size();
+        final int maxCharCount = lines // NOSONAR: always available
             .stream().map(String::length).max(Comparator.comparingInt(a -> a)).get();
         
-        // letters + empty space + 1 needed for padding  
-        final int width = Alphabet.LETTER_SIZE * lineCount + lineCount + 1;
-        final int height  = Alphabet.LETTER_SIZE * charCount + charCount + 1;
+        // letters + empty space + 1 needed for padding
+        // it's read up-down then left-right, so width and height are switched
+        final int width  = Alphabet.LETTER_SIZE * lineCount + lineCount + 1;
+        final int height = Alphabet.LETTER_SIZE * maxCharCount + maxCharCount + 1;
         
-        List<List<byte[][]>> convertedLines = lines
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        image.createGraphics().setBackground(new Color(SECONDARY_COLOUR));
+        
+        drawLines(convertedLines, image);
+        
+        final BufferedImage scaledImage = scaleImage(image);
+        
+        ImageIO.write(scaledImage, "PNG", new File("out.png"));
+    }
+    
+    private static BufferedImage scaleImage(final BufferedImage image) {
+        final AffineTransform transformation = new AffineTransform();
+        transformation.scale(SCALE, SCALE);
+        final AffineTransformOp operation = new AffineTransformOp(
+            transformation,
+            AffineTransformOp.TYPE_NEAREST_NEIGHBOR
+        );
+        return operation.filter(
+            image,
+            new BufferedImage(
+                image.getWidth() * SCALE,
+                image.getHeight() * SCALE,
+                BufferedImage.TYPE_INT_RGB
+            )
+        );
+    }
+    
+    private static void drawLines(
+        final List<List<byte[][]>> convertedLines,
+        final BufferedImage image
+    ) {
+        for (int x = 0; x < convertedLines.size(); x++) {
+            final List<byte[][]> currentLine = convertedLines.get(x);
+            for (int subLetterY = 0; subLetterY < Alphabet.LETTER_SIZE; subLetterY++) {
+                drawLine(currentLine, x, subLetterY, image);
+            }
+        }
+    }
+    
+    private static void drawLine(
+        final List<byte[][]> line,
+        final int x,
+        final int subLetterY,
+        final BufferedImage image
+    ) {
+        for (int y = 0; y < line.size(); y++) {
+            for (int subLetterX = 0; subLetterX < Alphabet.LETTER_SIZE; subLetterX++) {
+                final int xOffset = x * 6 + 1 + subLetterX;
+                final int yOffset = y * 6 + 1 + subLetterY;
+                
+                image.setRGB(
+                    xOffset,
+                    yOffset,
+                    line.get(y)[subLetterY][subLetterX] == 1 ? PRIMARY_COLOUR : SECONDARY_COLOUR
+                );
+            }
+        }
+    }
+    
+    private static List<List<byte[][]>> convertLines(final List<String> lines) {
+        return lines
             .stream()
             .map(String::toLowerCase)
             .map(String::chars)
             .map(stream -> stream
                 .boxed()
-                .map(c -> Alphabet.of((char) (c & 0xFF)))
-                .map(o -> o.orElseGet(() -> Alphabet.EMPTY))
+                .map(charValue -> Alphabet.of((char) (charValue & 0xFF)))
+                .map(optional -> optional.orElseGet(() -> Alphabet.EMPTY))
                 .toList())
             .toList();
-        
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        
-        for (int x = 0; x < convertedLines.size(); x++) {
-            final List<byte[][]> currentLine = convertedLines.get(x);
-            for (int iy = 0; iy < Alphabet.LETTER_SIZE; iy++) {
-                for (int y = 0; y < currentLine.size(); y++) {
-                    
-                    for (int ix = 0; ix < Alphabet.LETTER_SIZE; ix++) {
-                        final int xOffset = x * 6 + 1 + ix;
-                        final int yOffset = y * 6 + 1 + iy;
-                        image.setRGB(
-                            xOffset,
-                            yOffset,
-                            currentLine.get(y)[iy][ix] == 1 ? 0xFF0000 : 0x000000
-                        );
-                    }
-                }
-            }
-        }
-        
-        final int scale = 25;
-        
-        final AffineTransform transformation = new AffineTransform();
-        transformation.scale(scale, scale);
-        final AffineTransformOp operation = new AffineTransformOp(transformation, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
-        
-        ImageIO.write(operation.filter(image, new BufferedImage(image.getWidth() * scale, image.getHeight() * scale, BufferedImage.TYPE_INT_RGB)), "PNG", new File("out.png"));
     }
 }
